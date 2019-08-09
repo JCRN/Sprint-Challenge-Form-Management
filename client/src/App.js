4bd088822d0f99f5442d511a8d825adff2714af5
JCRN <clemons.jason@gmail.com>
2019-08-09 09:02:40 -0700

    built class App component with axios GET call

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      restricted: {}
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/restricted/data')
+      .then(response => response.data)
+      .then(data => {
+        this.setState({ restricted: data })
+        console.log(this.state.restricted)
+      })
+      .catch(error => console.log('GET Error: ', error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Login />
+      </div>
+    )
+  }
 }
 
 export default App
