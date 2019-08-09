83f3650c674119cf915a48806bbc02b99248029b
JCRN <clemons.jason@gmail.com>
2019-08-09 08:55:14 -0700

    built form class component

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import dependencies
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import styles
+import "./stylesheets/main.scss";
+import "semantic-ui-css/semantic.min.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import App
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
